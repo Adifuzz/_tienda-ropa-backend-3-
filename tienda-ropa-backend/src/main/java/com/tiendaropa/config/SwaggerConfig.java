7bc16deb0d963a8e5b1ace9e3fb326fad542ce23
“Adolfo <ado.medina.val2094@gmail.com>
2025-11-30 16:24:41 -0300

    se agrega swagger

---
+++ tienda-ropa-backend/src/main/java/com/tiendaropa/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.tiendaropa.config;
+
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API RopaPlus - Backend")
+                        .version("1.0")
+                        .description("Documentación oficial de la API REST para el sistema de gestión de RopaPlus.")
+                        .contact(new Contact()
+                                .name("Equipo de Desarrollo")
+                                .email("soporte@ropaplus.com")));
+    }
+}
